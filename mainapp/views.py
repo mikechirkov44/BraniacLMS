ffb4a6cf60d0b34dd6f960538f48eced355b1631
mikechirkov44 <ya.mikechirkov@yandex.ru>
2022-05-02 16:55:47 +0300

    Initialization of the Project on Django

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+# from urllib3 import HTTPResponse
+# from django.http import HttpResponse
+from tempfile import template
+
+from django.views.generic import TemplateView
 
 # Create your views here.
+
+
+class MainPageView(TemplateView):
+    template_name = "mainapp/index.html"
+
+
+class NewsPageView(TemplateView):
+    template_name = "mainapp/news.html"
+
+
+class CoursesPageView(TemplateView):
+    template_name = "mainapp/courses_list.html"
+
+
+class ContactsPageView(TemplateView):
+    template_name = "mainapp/contacts.html"
+
+
+class DocSitePageView(TemplateView):
+    template_name = "mainapp/doc_site.html"
+
+
+class LoginPageView(TemplateView):
+    template_name = "mainapp/login.html"
